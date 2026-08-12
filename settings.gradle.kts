12a93a1a42de88d761623177aabbf231f92a26b7
emilzhianshin-dot <emilzhianshin@gmail.com>
2026-08-12 04:01:21 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "NamazSafadzhay2026"
+include(":app")
