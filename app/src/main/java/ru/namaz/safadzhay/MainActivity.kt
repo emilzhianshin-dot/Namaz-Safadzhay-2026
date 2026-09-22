f54bb59b716698491d0ecfb0d44a76eb2af188a4
emilzhianshin-dot <emilzhianshin@gmail.com>
2026-09-22 13:52:53 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/namaz/safadzhay/MainActivity.kt
+++ app/src/main/java/ru/namaz/safadzhay/MainActivity.kt
@@ -1582,8 +1582,9 @@ if (needsNotificationPermission) {
             mainScroll.scrollTo(0, 0)
             newIntent.action = Intent.ACTION_MAIN
         }
+    }
 
-override fun onResume() {
+    override fun onResume() {
     super.onResume()
 
     if (::placeText.isInitialized) {
